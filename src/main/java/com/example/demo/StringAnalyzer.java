6ee646363363858846248c9bf4305fa3c2ebdb1c
Bilant.Andrzej <215679@edu.p.lodz.pl>
2020-10-27 14:47:42 +0100

    implemented String analyse for numbers, lowercase, uppercase and special sings

--- src/main/java/com/example/demo/StringAnalyzer.java
+++ src/main/java/com/example/demo/StringAnalyzer.java
@@ -9,7 +9,31 @@ public class StringAnalyzer {
 
     @RequestMapping(path = "/analyseString")
     public String analyseString(@RequestParam(value = "stringToanalyse", required = false)String input){
-        return "test";
+
+        StringBuilder result = new StringBuilder();
+
+        result.append("In the input were:\n");
+
+        char[] chars = input.toCharArray();
+
+        int upperCase = 0, lowerCase = 0, numbers = 0, specialSings = 0;
+
+        for(char c : chars){
+
+            if(Character.isDigit(c)){
+                numbers++;
+            }
+            else if(Character.isUpperCase(c)){
+                upperCase++;
+            }
+            else if(Character.isLowerCase(c)){
+                lowerCase++;
+            }else{
+                specialSings++;
+            }
+        }
+
+        return result.toString();
     }
 
 }
