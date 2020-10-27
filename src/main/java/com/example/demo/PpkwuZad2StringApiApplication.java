45b9a61fb95625c23146f73d816b575ac45d6652
Bilant.Andrzej <215679@edu.p.lodz.pl>
2020-10-27 14:27:00 +0100

    Initial commit - generated spring project from spring initializer

---
+++ src/main/java/com/example/demo/PpkwuZad2StringApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpkwuZad2StringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpkwuZad2StringApiApplication.class, args);
+	}
+
+}
