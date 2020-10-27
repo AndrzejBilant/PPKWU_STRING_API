b88627f279baafe83d14d1b18d8334340b4453af
Bilant.Andrzej <215679@edu.p.lodz.pl>
2020-10-27 14:34:20 +0100

    created analyseString class

--- src/main/java/com/example/demo/StringAnalyzer.java
+++ src/main/java/com/example/demo/StringAnalyzer.java
@@ -1,8 +1,15 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StringAnalyzer {
+
+    @RequestMapping(path = "/analyseString")
+    public String analyseString(@RequestParam(value = "stringToanalyse", required = false)String input){
+        return "test";
+    }
 
 }
